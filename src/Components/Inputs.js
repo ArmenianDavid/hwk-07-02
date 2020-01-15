375abeea149d9bfb48c2fd57f78ce1bc45407bfb
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-15 19:44:10 +0400

    created state and Input component

---
+++ src/Components/Inputs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Input = (props) =>{
+   return(
+     <div className='input-part'>
+       <div className='left-input-container'>
+          <input type='text' />
+       </div>
+
+       <div className='right-input-container'>
+          <input type='text' />
+       </div>
+     </div>
+   )
+}
+
+export default Input
--- src/Task.js
+++ src/Task.js
@@ -1,19 +1,24 @@
 import React from 'react'
 // Components
 import Header from './Components/Header'
+import Inputs from './Components/Inputs'
  
 class Task extends React.Component{
   constructor(){
     super()
     this.state = {
-
+       inputColor : '',
+       inputBgColor : '',
     }
   }
+
+
 
   render(){
     return(
       <>
        <Header />
+       <Inputs />
       </>
     )
   }
