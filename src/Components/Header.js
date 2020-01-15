d49a369b867c6c93d57c25455f859e7469cba451
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-15 19:22:41 +0400

    Initial commit

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+// Compornents
+import './Header.css'
+
+const Header = () =>{
+  return(
+    <div className='header-container'>
+      <div>
+        <h1 className='header-text'>Color changing page!!</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/Task.js
@@ -0,0 +1,22 @@
+import React from 'react'
+// Components
+import Header from './Components/Header'
+ 
+class Task extends React.Component{
+  constructor(){
+    super()
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <>
+       <Header />
+      </>
+    )
+  }
+}
+
+export default Task
