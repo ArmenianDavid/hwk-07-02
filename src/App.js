c485d69e7f73069164fe5526459b0d4026b28250
Davit Sahakyan <sahakyandavit92@gmail.com>
2020-01-20 16:41:16 +0400

    style : formated with prettier

--- src/App.js
+++ src/App.js
@@ -3,36 +3,40 @@ import React from "react";
 import Header from "./Components/Header";
 import Inputs from "./Components/Inputs";
 import TextPart from "./Components/TextPart";
-import PoweredBy from './Components/PoweredBy/PoweredBy'
+import PoweredBy from "./Components/PoweredBy/PoweredBy";
 
 class App extends React.Component {
     constructor() {
         super();
         this.state = {
-            inputColor: localStorage.getItem('inputColor') ? localStorage.getItem('inputColor') : '' ,
-            inputBgColor: localStorage.getItem('inputBgColor') ? localStorage.getItem('inputBgColor') : '',
+            inputColor: localStorage.getItem("inputColor")
+                ? localStorage.getItem("inputColor")
+                : "",
+            inputBgColor: localStorage.getItem("inputBgColor")
+                ? localStorage.getItem("inputBgColor")
+                : ""
         };
     }
-    
-    handleInput = ( id , input) =>{
-        if( id === 'inputColor'){
-            localStorage.setItem('inputColor' , input)
-           this.setState({ inputColor : input})
-          } else if(id === 'inputBgColor'){
-            localStorage.setItem('inputBgColor' , input)
-            this.setState({ inputBgColor : input})
-          }
-    }
+
+    handleInput = (id, input) => {
+        if (id === "inputColor") {
+            localStorage.setItem("inputColor", input);
+            this.setState({ inputColor: input });
+        } else if (id === "inputBgColor") {
+            localStorage.setItem("inputBgColor", input);
+            this.setState({ inputBgColor: input });
+        }
+    };
 
     render() {
-        const { inputColor, inputBgColor , isValid } = this.state;
-        console.log(isValid)
+        const { inputColor, inputBgColor, isValid } = this.state;
+        console.log(isValid);
         return (
             <>
                 <Header title="color changing page!!" />
                 <Inputs handleInput={this.handleInput} />
-                <TextPart color={inputColor} bgColor={inputBgColor}  />
-                <PoweredBy text='Powered By' name='Davit Sahakyan' />
+                <TextPart color={inputColor} bgColor={inputBgColor} />
+                <PoweredBy text="Powered By" name="Davit Sahakyan" />
             </>
         );
     }
--- src/Components/Inputs.js
+++ src/Components/Inputs.js
@@ -1,74 +1,97 @@
-import React from 'react'
+import React from "react";
 // Components
-import './Inputs.css'
+import "./Inputs.css";
 
+class Input extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputColor: "",
+            inputBgColor: "",
+            isValidColor: true,
+            isValidBgColor: true
+        };
+    }
 
-class Input extends React.Component{
-   constructor(props){
-      super(props)
-      this.state ={
-         inputColor : '',
-         inputBgColor : '',
-         isValidColor : true ,
-         isValidBgColor : true ,
-      }
-   }
-   
+    handleInputChange = event => {
+        if (event.target.id === "inputColor") {
+            let isOk =
+                /^#([0-9A-F]{3}){1,2}$/i.test(event.target.value) ||
+                event.target.value === "";
+            return this.setState(
+                { isValidColor: isOk, inputColor: event.target.value },
+                () =>
+                    this.props.handleInput("inputColor", this.state.inputColor)
+            );
+        } else if (event.target.id === "inputBgColor") {
+            let isOk =
+                /^#([0-9A-F]{3}){1,2}$/i.test(event.target.value) ||
+                event.target.value === "";
+            return this.setState(
+                { isValidBgColor: isOk, inputBgColor: event.target.value },
+                () =>
+                    this.props.handleInput(
+                        "inputBgColor",
+                        this.state.inputBgColor
+                    )
+            );
+        }
+    };
 
-   handleInputChange = event => {
+    render() {
+        const { isValidColor, isValidBgColor } = this.state;
+        console.log(localStorage);
 
-      if (event.target.id === "inputColor") {
-         let isOk = /^#([0-9A-F]{3}){1,2}$/i.test(event.target.value) || event.target.value  === '';
-        return this.setState({ isValidColor : isOk , inputColor: event.target.value},
-            ()=>this.props.handleInput("inputColor" , this.state.inputColor )
-            ) 
-         
-      } else if (event.target.id === "inputBgColor" ) {
-         let isOk = /^#([0-9A-F]{3}){1,2}$/i.test(event.target.value) || event.target.value === '';
-        return this.setState({ isValidBgColor : isOk , inputBgColor: event.target.value},
-            ()=>this.props.handleInput("inputBgColor" , this.state.inputBgColor )
-            )
-      }
-  };
+        return (
+            <div className="input-part">
+                <div className="left-input-container">
+                    Color :
+                    <div>
+                        <input
+                            value={
+                                localStorage.getItem("inputColor")
+                                    ? localStorage.getItem("inputColor")
+                                    : ""
+                            }
+                            style={{
+                                border: `1px solid ${
+                                    isValidColor ? "green" : "red"
+                                }`
+                            }}
+                            type="text"
+                            className="input"
+                            id="inputColor"
+                            placeholder="Start with #"
+                            onChange={e => this.handleInputChange(e)}
+                        />
+                    </div>
+                </div>
 
-
-   render(){
-     const  { isValidColor , isValidBgColor } = this.state
-     console.log(localStorage)
-
-   return(
-     <div className='input-part'>
-       <div className='left-input-container'>
-          Color :
-          <div>
-             <input 
-             value={localStorage.getItem('inputColor') ? localStorage.getItem('inputColor') : ''}
-             style={{ border : `1px solid ${isValidColor ? 'green' : 'red'}`}}
-             type='text' 
-             className='input' 
-             id='inputColor' 
-             placeholder='Start with #' 
-             onChange={(e)=>this.handleInputChange(e )}/>
-          </div>
-       </div>
-
-       <div className='right-input-container'>
-          BgColor :
-          <div>
-             <input 
-             value={localStorage.getItem('inputBgColor') ? localStorage.getItem('inputBgColor') : ''}
-             style={{ border : `1px solid ${isValidBgColor ? 'green' : 'red'}`}}
-             type='text' 
-             className='input'
-             id='inputBgColor'
-             placeholder='Start with #' 
-             onChange={(e)=>this.handleInputChange(e )} />
-          </div>
-       </div>
-     </div>
-   ) 
-   }
-   
+                <div className="right-input-container">
+                    BgColor :
+                    <div>
+                        <input
+                            value={
+                                localStorage.getItem("inputBgColor")
+                                    ? localStorage.getItem("inputBgColor")
+                                    : ""
+                            }
+                            style={{
+                                border: `1px solid ${
+                                    isValidBgColor ? "green" : "red"
+                                }`
+                            }}
+                            type="text"
+                            className="input"
+                            id="inputBgColor"
+                            placeholder="Start with #"
+                            onChange={e => this.handleInputChange(e)}
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
-export default Input
+export default Input;
